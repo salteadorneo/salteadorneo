4ee66a3e9db064cefeb4953686f3a123d3124877
Cristian Adán <salteadorneo@users.noreply.github.com>
2022-05-26 18:48:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { promises as fs } from 'fs'
   const [template] = await Promise.all([
     fs.readFile('./src/README.md.tpl', { encoding: 'utf-8' }),
   ])
+  
+  template = template.replace(/%{{random}}%/g, Math.round(Math.random() * 100) / 10);
 
   await fs.writeFile('README.md', template)
 })()
